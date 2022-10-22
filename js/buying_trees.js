0b970b46b14c82415f1fcabd7e5f71144ed78189
edwin <eddiendiritu@gmail.com>
2022-10-22 12:36:02 +0300

    another commit

--- js/buying_trees.js
+++ js/buying_trees.js
@@ -19,8 +19,12 @@ fetch(`http://18.182.31.147/api/treeshop/${shopID}`, requestOptions)
   .then((response) => response.json())
   .then((result) => {
     var trees = result["trees"];
+    document.getElementsByTagName('title').innerHTML=result['name']
+    var shop_name = document.getElementsByClassName('shop_name')[0]
+    console.log(shop_name)
+    shop_name.innerHTML = result['name']
+    console.log(result['name'])
     trees.forEach((element) => {
-      console.log(element);
 
       var plantCard = document.createElement("div");
       plantCard.classList.add("popular");
