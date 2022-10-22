2b6a4496070aa97cb4040c363a4aafcf29b25b9c
edwin <eddiendiritu@gmail.com>
2022-10-22 12:46:14 +0300

    another change

---
+++ js/nav_bar.js
@@ -0,0 +1,28 @@
+var header = document.getElementsByTagName('header')[0]
+
+header.innerHTML = `<nav>
+<div class="logo">
+  <img src="images/logo.jpg" alt="" />
+  <span>MR BRIGHTSIDE</span>
+</div>
+<div id="navshow">
+  <i class="fa-solid fa-bars"></i>
+</div>
+</nav>
+<div class="navlinks" style="display: none" id="navlinks">
+<div id="navhide"><i class="fa-solid fa-bars"></i></div>
+<ul>
+  <li><a href="index.html">home</a></li>
+  <li><a href="bins.html">Waste management</a></li>
+  <li><a href="buyingtrees.html">trees</a></li>
+  <li><a href="near-you-treesshop.html">locate treeshop</a></li>
+  <li><a href="campaigns.html">campaigns</a></li>
+  <li><a href="near-you-campaigns.html">Near you campaigns</a></li>
+  <li><a href="report.html">report</a></li>
+  <li><a href="cart.html">cart</a></li>
+  <li><a href="info.html">info</a></li>
+  <li><a href="profile.html">profile</a></li>
+  <li><a href="register.html">register</a></li>
+  <li><a href="login.html">login</a></li>
+</ul>
+</div>`;
